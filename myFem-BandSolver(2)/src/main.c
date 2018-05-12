8570af275e9c47d35c2a899764e00570a5ddf39d
agobeaux <alexandre.gobeaux@student.uclouvain.be>
2018-05-12 17:59:59 +0200

    c

--- myFem-BandSolver(2)/src/main.c
+++ myFem-BandSolver(2)/src/main.c
@@ -32,7 +32,6 @@ int main(void)
 
     femSolverType solverType = FEM_ITER;
     femRenumType  renumType  = FEM_YNUM;
-<<<<<<< HEAD
     char meshFileName[] = "../data/meshMedium.txt";  
 
 // Pour Windows, remplacer l'argument :
@@ -43,18 +42,6 @@ int main(void)
 // Sorry for the inconvenience :-)
 // On réfléchit pour rendre cela plus transparent dans les homeworks suivants :-)
 // Be patient !
-=======
-    char meshFileName[] = "../data/rect_quad_1601.txt";
-
-    // Pour Windows, remplacer l'argument :
-    // ("../data/rect_quad_1601.txt")
-    // par :
-    // ("..\\data\\rect_quad_1601.txt")
-    //
-    // Sorry for the inconvenience :-)
-    // On rÃ©flÃ©chit pour rendre cela plus transparent dans les homeworks suivants :-)
-    // Be patient !
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
 
 
 
@@ -63,37 +50,23 @@ int main(void)
     femDiffusionCompute(theProblem);
     int testConvergence;
     do {
-<<<<<<< HEAD
-        femDiffusionCompute(theProblem);  
-        femSolverPrintInfos(theProblem->solver); 
-        testConvergence = femSolverConverged(theProblem->solver); 
-=======
         femDiffusionCompute(theProblem);
         femSolverPrintInfos(theProblem->solver);
         testConvergence = femSolverConverged(theProblem->solver);
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
     }
     while ( testConvergence == 0);
     printf("    CPU time : %.2f [sec] \n", (clock() - tic) * 1.0 /CLOCKS_PER_SEC);
     printf("    Maximum value : %.4f\n", femMax(theProblem->soluce,theProblem->size));
     fflush(stdout);
 
-
-<<<<<<< HEAD
-    int option = 1;     
-=======
+   
     int option = 1;
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
     femRenumType  newRenumType  = renumType;
 
     GLFWwindow* window = glfemInit("MECA1120 : homework 5 ");
     glfwMakeContextCurrent(window);
 
-<<<<<<< HEAD
     do 
-=======
-    do
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
     {
         int w,h;
         char theMessage[256];
@@ -102,17 +75,6 @@ int main(void)
 
         if (option == 1) {
             glfemReshapeWindows(theProblem->mesh,w,h);
-<<<<<<< HEAD
-            glfemPlotField(theProblem->mesh,theProblem->soluce);   
-        }
-        else {
-            glColor3f(1.0,0.0,0.0);
-            glfemPlotSolver(theProblem->solver,theProblem->size,w,h); 
-        }
-        glColor3f(0.0,0.0,0.0); glfemDrawMessage(20,460,theMessage);              
-
-        if (renumType != newRenumType) {             
-=======
             glfemPlotField(theProblem->mesh,theProblem->soluce);
         }
         else {
@@ -123,49 +85,19 @@ int main(void)
         glfemDrawMessage(20,460,theMessage);
 
         if (renumType != newRenumType) {
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
             renumType = newRenumType;
             femDiffusionFree(theProblem);
             theProblem = femDiffusionCreate(meshFileName,solverType,renumType);
             clock_t tic = clock();
             do {
-<<<<<<< HEAD
                 femDiffusionCompute(theProblem);  
                 femSolverPrintInfos(theProblem->solver); 
                 testConvergence = femSolverConverged(theProblem->solver); 
-=======
-                femDiffusionCompute(theProblem);
-                femSolverPrintInfos(theProblem->solver);
-                testConvergence = femSolverConverged(theProblem->solver);
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
             }
             while ( testConvergence == 0);
             if (testConvergence == -1)  printf("    Iterative solver stopped afer a maximum number of iterations\n");
             printf("    CPU time : %.2f [sec] \n", (clock() - tic) * 1.0 /CLOCKS_PER_SEC);
             switch (renumType) {
-<<<<<<< HEAD
-                case FEM_XNUM : printf("    Renumbering along the x-direction\n"); break;
-                case FEM_YNUM : printf("    Renumbering along the y-direction\n"); break;
-                default : break; 
-            }
-            printf("    Maximum value : %.4f\n", femMax(theProblem->soluce,theProblem->size));
-            fflush(stdout); 
-        }
-        if (glfwGetKey(window,'V') == GLFW_PRESS)   option = 1;
-        if (glfwGetKey(window,'S') == GLFW_PRESS)   option = 0; 
-        if (glfwGetKey(window,'X') == GLFW_PRESS)   newRenumType  = FEM_XNUM; 
-        if (glfwGetKey(window,'Y') == GLFW_PRESS)   newRenumType  = FEM_YNUM; 
-        if (glfwGetKey(window,'N') == GLFW_PRESS)   newRenumType  = FEM_NO; 
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    } 
-    while( glfwGetKey(window,GLFW_KEY_ESCAPE) != GLFW_PRESS && glfwWindowShouldClose(window) != 1 );
-
-// Check if the ESC key was pressed or the window was closed
-
-    glfwTerminate(); 
-=======
             case FEM_XNUM :
                 printf("    Renumbering along the x-direction\n");
                 break;
@@ -192,15 +124,8 @@ int main(void)
     // Check if the ESC key was pressed or the window was closed
 
     glfwTerminate();
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
     femDiffusionFree(theProblem);
     exit(EXIT_SUCCESS);
 
-
-<<<<<<< HEAD
-
-}
-=======
->>>>>>> 892f4706b2c091bd45fab6dc95d4a32aa05e66b1
 
 }
